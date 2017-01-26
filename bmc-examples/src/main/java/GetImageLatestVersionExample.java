30b30777fe0b193f7dbce376158725cc5d04a242
Joe Levy <joe.levy@oracle.com>
2017-01-25 17:13:43 -0800

    adding example for getting latest image version

---
+++ bmc-examples/src/main/java/GetImageLatestVersionExample.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2016-2017 Oracle and/or its affiliates. All rights reserved.
+ */
+import java.util.List;
+
+import com.oracle.bmc.Region;
+import com.oracle.bmc.auth.AuthenticationDetailsProvider;
+import com.oracle.bmc.auth.ConfigFileAuthenticationDetailsProvider;
+import com.oracle.bmc.core.ComputeClient;
+import com.oracle.bmc.core.model.Image;
+import com.oracle.bmc.core.requests.ListImagesRequest;
+import com.oracle.bmc.core.responses.ListImagesResponse;
+
+public class GetImageLatestVersionExample {
+
+    public static void main(String[] args) throws Exception {
+
+        // TODO: Fill in these values
+        Region region = Region.US_PHOENIX_1;
+        String operatingSystem = "Oracle Linux";
+        String operatingSystemVersion = "6.7";
+        
+        String configurationFilePath = "~/.oraclebmc/config";
+        String profile = "DEFAULT";
+
+        AuthenticationDetailsProvider provider =
+                new ConfigFileAuthenticationDetailsProvider(configurationFilePath, profile);
+
+        // Find matching images
+        ComputeClient computeClient = new ComputeClient(provider);
+        computeClient.setRegion(region);
+        
+        ListImagesResponse listImagesResponse = computeClient.listImages(
+                ListImagesRequest.builder()
+                    .compartmentId(provider.getTenantId())
+                    .operatingSystem(operatingSystem)
+                    .operatingSystemVersion(operatingSystemVersion)
+                    .build());
+        
+        computeClient.close();
+        
+        List<Image> matchingImages = listImagesResponse.getItems();
+        
+        if(matchingImages.size() == 0) {
+            System.out.println("No matching images found.");
+        }
+        else {
+            /*
+             * Images are sorted by system images first, then custom images, secondarily sorted by image display name.
+             * Since image display names take the format of "<OS>-<OSVersion>-<Year>.<Month>.<Day>-<Iteration>",
+             * the first image returned for a list operation sorted by OS and OS version will be the the latest system
+             * image for that OS / OS version.
+             */
+            Image latestImageVersion = matchingImages.get(0);
+            
+            System.out.println(latestImageVersion);
+        }
+    }
+}
