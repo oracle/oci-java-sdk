c38f3de3225f2061ec1ea69199339a6bda7231b9
DEXREQ Automation <noreply+dexreq@oracle.com>
2023-01-31 19:20:32 +0000

    Committing source formatting changes. Automatically performed, see TeamCity build log:
    https://teamcity.oci.oraclecorp.com/viewLog.html?buildId=36925036&buildTypeId=Sdk_OCI_Testing_Service_BuildPullRequest&tab=buildResultsDiv

--- bmc-waas/src/main/java/com/oracle/bmc/waas/requests/UpdateGoodBotsRequest.java
+++ bmc-waas/src/main/java/com/oracle/bmc/waas/requests/UpdateGoodBotsRequest.java
@@ -16,12 +16,14 @@ public class UpdateGoodBotsRequest
                 java.util.List<com.oracle.bmc.waas.model.GoodBot>> {
 
     /**
-     * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the WAAS policy.
+     * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the
+     * WAAS policy.
      */
     private String waasPolicyId;
 
     /**
-     * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the WAAS policy.
+     * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the
+     * WAAS policy.
      */
     public String getWaasPolicyId() {
         return waasPolicyId;
@@ -100,14 +102,14 @@ public class UpdateGoodBotsRequest
         private com.oracle.bmc.retrier.RetryConfiguration retryConfiguration = null;
 
         /**
-         * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the WAAS
-         * policy.
+         * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the
+         * WAAS policy.
          */
         private String waasPolicyId = null;
 
         /**
-         * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the WAAS
-         * policy.
+         * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the
+         * WAAS policy.
          *
          * @param waasPolicyId the value to set
          * @return this builder instance
