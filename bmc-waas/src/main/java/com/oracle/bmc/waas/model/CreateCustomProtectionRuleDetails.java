b6548226d6b06a4a2a716dbe3d27018392a7d669
Josh Hunter <josh.hunter@oracle.com>
2023-02-21 10:54:25 -0800

    Revert "Committing source formatting changes. Automatically performed, see TeamCity build log:"
    This reverts commit 63115f9ce24ffa09f9995d1c9fd37e224cc3c5c1.

--- bmc-waas/src/main/java/com/oracle/bmc/waas/model/CreateCustomProtectionRuleDetails.java
+++ bmc-waas/src/main/java/com/oracle/bmc/waas/model/CreateCustomProtectionRuleDetails.java
@@ -125,8 +125,8 @@ public final class CreateCustomProtectionRuleDetails
          * part 2/2" \\ "phase:2, \\ msg:'Detects chained SQL injection attempts 2/2.', \\ id:
          * {{id_2}}, \\ ctl:ruleEngine={{mode}}, \\ deny" }{@code <p>
          *
-         * <p><p><p><p>The example contains two }SecRules{@code each having distinct regex
-         * expression to match the }Cookie header value during the second input analysis phase.
+         * <p><p><p>The example contains two }SecRules{@code each having distinct regex expression
+         * to match the }Cookie header value during the second input analysis phase.
          *
          * <p>For more information about custom protection rules, see [Custom Protection
          * Rules](https://docs.cloud.oracle.com/Content/WAF/Tasks/customprotectionrules.htm).
@@ -165,8 +165,8 @@ public final class CreateCustomProtectionRuleDetails
          * part 2/2" \\ "phase:2, \\ msg:'Detects chained SQL injection attempts 2/2.', \\ id:
          * {{id_2}}, \\ ctl:ruleEngine={{mode}}, \\ deny" }{@code <p>
          *
-         * <p><p><p><p>The example contains two }SecRules{@code each having distinct regex
-         * expression to match the }Cookie header value during the second input analysis phase.
+         * <p><p><p>The example contains two }SecRules{@code each having distinct regex expression
+         * to match the }Cookie header value during the second input analysis phase.
          *
          * <p>For more information about custom protection rules, see [Custom Protection
          * Rules](https://docs.cloud.oracle.com/Content/WAF/Tasks/customprotectionrules.htm).
@@ -356,7 +356,7 @@ public final class CreateCustomProtectionRuleDetails
      * part 2/2" \\ "phase:2, \\ msg:'Detects chained SQL injection attempts 2/2.', \\ id: {{id_2}},
      * \\ ctl:ruleEngine={{mode}}, \\ deny" }{@code <p>
      *
-     * <p><p><p><p>The example contains two }SecRules{@code each having distinct regex expression to
+     * <p><p><p>The example contains two }SecRules{@code each having distinct regex expression to
      * match the }Cookie header value during the second input analysis phase.
      *
      * <p>For more information about custom protection rules, see [Custom Protection
@@ -395,7 +395,7 @@ public final class CreateCustomProtectionRuleDetails
      * part 2/2" \\ "phase:2, \\ msg:'Detects chained SQL injection attempts 2/2.', \\ id: {{id_2}},
      * \\ ctl:ruleEngine={{mode}}, \\ deny" }{@code <p>
      *
-     * <p><p><p><p>The example contains two }SecRules{@code each having distinct regex expression to
+     * <p><p><p>The example contains two }SecRules{@code each having distinct regex expression to
      * match the }Cookie header value during the second input analysis phase.
      *
      * <p>For more information about custom protection rules, see [Custom Protection
