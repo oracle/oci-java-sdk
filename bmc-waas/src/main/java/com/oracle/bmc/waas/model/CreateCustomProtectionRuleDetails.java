b91606fcf44ac91bef21165714251ca9b2e31606
DEXREQ Automation <noreply+dexreq@oracle.com>
2023-04-25 16:36:55 +0000

    Committing source formatting changes. Automatically performed, see TeamCity build log:
    https://teamcity.oci.oraclecorp.com/viewLog.html?buildId=38553021&buildTypeId=Sdk_OCI_Testing_Service_BuildPullRequest&tab=buildResultsDiv

--- bmc-waas/src/main/java/com/oracle/bmc/waas/model/CreateCustomProtectionRuleDetails.java
+++ bmc-waas/src/main/java/com/oracle/bmc/waas/model/CreateCustomProtectionRuleDetails.java
@@ -7,9 +7,9 @@ package com.oracle.bmc.waas.model;
 /**
  * The required data to create a custom protection rule. For more information about custom
  * protection rules, see [Custom Protection
- * Rules](https://docs.cloud.oracle.com/iaas/Content/WAF/Tasks/customprotectionrules.htm). **Warning:** Oracle
- * recommends that you avoid using any confidential information when you supply string values using
- * the API. <br>
+ * Rules](https://docs.cloud.oracle.com/iaas/Content/WAF/Tasks/customprotectionrules.htm).
+ * **Warning:** Oracle recommends that you avoid using any confidential information when you supply
+ * string values using the API. <br>
  * Note: Objects should always be created or deserialized using the {@link Builder}. This model
  * distinguishes fields that are {@code null} because they are unset from fields that are explicitly
  * set to {@code null}. This is done in the setter methods of the {@link Builder}, which maintain a
@@ -290,15 +290,15 @@ public final class CreateCustomProtectionRuleDetails
     }
 
     /**
-     * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the compartment in
-     * which to create the custom protection rule.
+     * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the
+     * compartment in which to create the custom protection rule.
      */
     @com.fasterxml.jackson.annotation.JsonProperty("compartmentId")
     private final String compartmentId;
 
     /**
-     * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the compartment in
-     * which to create the custom protection rule.
+     * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the
+     * compartment in which to create the custom protection rule.
      *
      * @return the value
      */
--- bmc-waas/src/main/java/com/oracle/bmc/waas/model/CustomProtectionRule.java
+++ bmc-waas/src/main/java/com/oracle/bmc/waas/model/CustomProtectionRule.java
@@ -61,15 +61,15 @@ public final class CustomProtectionRule
     @com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder(withPrefix = "")
     public static class Builder {
         /**
-         * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the custom
-         * protection rule.
+         * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the
+         * custom protection rule.
          */
         @com.fasterxml.jackson.annotation.JsonProperty("id")
         private String id;
 
         /**
-         * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the custom
-         * protection rule.
+         * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the
+         * custom protection rule.
          *
          * @param id the value to set
          * @return this builder
@@ -80,15 +80,15 @@ public final class CustomProtectionRule
             return this;
         }
         /**
-         * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the custom
-         * protection rule's compartment.
+         * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the
+         * custom protection rule's compartment.
          */
         @com.fasterxml.jackson.annotation.JsonProperty("compartmentId")
         private String compartmentId;
 
         /**
-         * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the custom
-         * protection rule's compartment.
+         * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the
+         * custom protection rule's compartment.
          *
          * @param compartmentId the value to set
          * @return this builder
@@ -384,15 +384,15 @@ public final class CustomProtectionRule
     }
 
     /**
-     * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the custom
-     * protection rule.
+     * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the
+     * custom protection rule.
      */
     @com.fasterxml.jackson.annotation.JsonProperty("id")
     private final String id;
 
     /**
-     * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the custom
-     * protection rule.
+     * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the
+     * custom protection rule.
      *
      * @return the value
      */
@@ -401,15 +401,15 @@ public final class CustomProtectionRule
     }
 
     /**
-     * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the custom
-     * protection rule's compartment.
+     * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the
+     * custom protection rule's compartment.
      */
     @com.fasterxml.jackson.annotation.JsonProperty("compartmentId")
     private final String compartmentId;
 
     /**
-     * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the custom
-     * protection rule's compartment.
+     * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the
+     * custom protection rule's compartment.
      *
      * @return the value
      */
