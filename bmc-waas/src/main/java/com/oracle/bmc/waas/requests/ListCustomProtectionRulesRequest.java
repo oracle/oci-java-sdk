5a246addc5668a59384c75e964a18b2fc661c6f0
DEXREQ Automation <noreply+dexreq@oracle.com>
2023-05-09 18:30:40 +0000

    Committing source formatting changes. Automatically performed, see TeamCity build log:
    https://teamcity.oci.oraclecorp.com/viewLog.html?buildId=38900374&buildTypeId=Sdk_OCI_Testing_Service_BuildPullRequest&tab=buildResultsDiv

--- bmc-waas/src/main/java/com/oracle/bmc/waas/requests/ListCustomProtectionRulesRequest.java
+++ bmc-waas/src/main/java/com/oracle/bmc/waas/requests/ListCustomProtectionRulesRequest.java
@@ -16,14 +16,14 @@ public class ListCustomProtectionRulesRequest
         extends com.oracle.bmc.requests.BmcRequest<java.lang.Void> {
 
     /**
-     * The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the
-     * compartment. This number is generated when the compartment is created.
+     * The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of
+     * the compartment. This number is generated when the compartment is created.
      */
     private String compartmentId;
 
     /**
-     * The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the
-     * compartment. This number is generated when the compartment is created.
+     * The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of
+     * the compartment. This number is generated when the compartment is created.
      */
     public String getCompartmentId() {
         return compartmentId;
@@ -211,14 +211,14 @@ public class ListCustomProtectionRulesRequest
         private com.oracle.bmc.retrier.RetryConfiguration retryConfiguration = null;
 
         /**
-         * The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the
-         * compartment. This number is generated when the compartment is created.
+         * The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm)
+         * of the compartment. This number is generated when the compartment is created.
          */
         private String compartmentId = null;
 
         /**
-         * The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the
-         * compartment. This number is generated when the compartment is created.
+         * The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm)
+         * of the compartment. This number is generated when the compartment is created.
          *
          * @param compartmentId the value to set
          * @return this builder instance
