cf5b277a473676052bbe8689b7d2e9d488db08bc
DEXREQ Automation <noreply+dexreq@oracle.com>
2023-10-17 15:26:42 +0000

    Committing source formatting changes. Automatically performed, see TeamCity build log:
    https://teamcity.oci.oraclecorp.com/viewLog.html?buildId=43046437&buildTypeId=Sdk_OCI_Testing_Service_BuildPullRequest&tab=buildResultsDiv

--- bmc-waas/src/main/java/com/oracle/bmc/waas/requests/ListProtectionRulesRequest.java
+++ bmc-waas/src/main/java/com/oracle/bmc/waas/requests/ListProtectionRulesRequest.java
@@ -14,12 +14,14 @@ import com.oracle.bmc.waas.model.*;
 public class ListProtectionRulesRequest extends com.oracle.bmc.requests.BmcRequest<java.lang.Void> {
 
     /**
-     * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the WAAS policy.
+     * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the
+     * WAAS policy.
      */
     private String waasPolicyId;
 
     /**
-     * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the WAAS policy.
+     * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the
+     * WAAS policy.
      */
     public String getWaasPolicyId() {
         return waasPolicyId;
@@ -114,14 +116,14 @@ public class ListProtectionRulesRequest extends com.oracle.bmc.requests.BmcReque
         private com.oracle.bmc.retrier.RetryConfiguration retryConfiguration = null;
 
         /**
-         * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the WAAS
-         * policy.
+         * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the
+         * WAAS policy.
          */
         private String waasPolicyId = null;
 
         /**
-         * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the WAAS
-         * policy.
+         * The [OCID](https://docs.cloud.oracle.com/Content/General/Concepts/identifiers.htm) of the
+         * WAAS policy.
          *
          * @param waasPolicyId the value to set
          * @return this builder instance
