326db6d1fb7e4c5c8cb9f9b84c4588989995f800
Cameron Bateman <cameron.bateman@oracle.com>
2024-07-25 01:46:19 -0700

    Fixed location of test data class and removed additional header info.
    Signed-off-by: Cameron Bateman <cameron.bateman@oracle.com>

--- bmc-common/src/main/java/com/oracle/bmc/ConfigFileReader.java
+++ bmc-common/src/main/java/com/oracle/bmc/ConfigFileReader.java
@@ -1,10 +1,6 @@
 /**
  * Copyright (c) 2016, 2024, Oracle and/or its affiliates.  All rights reserved.
  * This software is dual-licensed to you under the Universal Permissive License (UPL) 1.0 as shown at https://oss.oracle.com/licenses/upl or Apache License 2.0 as shown at http://www.apache.org/licenses/LICENSE-2.0. You may choose either license.
-
- * C.B 2024: copied from OCI SDK, copyright notice unchanged.  We will try to patch this change back into
- *           OCI (https://github.com/oracle/oci-java-sdk).  This change exposes the profile names and key/value 
- *           pairs that are already being parsed in. This is needed by tools (IntelliJ) and admin tools.
  */
 package com.oracle.bmc;
 
